42531364712d5b6ba85792993194423b91676f49
R.9240 <59245961+rika-9240@users.noreply.github.com>
2021-04-03 18:39:23 +0900

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace TimerF20210403
+{
+    partial class Timer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TimeView = new System.Windows.Forms.Label();
+            this.StartStop = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // TimeView
+            // 
+            this.TimeView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TimeView.AutoSize = true;
+            this.TimeView.Font = new System.Drawing.Font("メイリオ", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.TimeView.Location = new System.Drawing.Point(42, 20);
+            this.TimeView.Name = "TimeView";
+            this.TimeView.Size = new System.Drawing.Size(147, 41);
+            this.TimeView.TabIndex = 0;
+            this.TimeView.Text = "TimeView";
+            // 
+            // StartStop
+            // 
+            this.StartStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.StartStop.Location = new System.Drawing.Point(30, 72);
+            this.StartStop.Name = "StartStop";
+            this.StartStop.Size = new System.Drawing.Size(75, 75);
+            this.StartStop.TabIndex = 1;
+            this.StartStop.Text = "Start";
+            this.StartStop.UseVisualStyleBackColor = true;
+            this.StartStop.Click += new System.EventHandler(this.StartStop_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Reset.Location = new System.Drawing.Point(130, 72);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 75);
+            this.Reset.TabIndex = 2;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Timer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 161);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.StartStop);
+            this.Controls.Add(this.TimeView);
+            this.Name = "Timer";
+            this.Text = "Timer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TimeView;
+        private System.Windows.Forms.Button StartStop;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerF20210403
+{
+    public partial class Timer : Form
+    {
+        public Timer()
+        {
+            InitializeComponent();
+        }
+        TimeSpan timeNow = new TimeSpan(0,0,0);
+        TimeSpan addTime = new TimeSpan(0, 0, 1);
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeNow += addTime;
+            TimeView.Text = timeNow.ToString();
+
+
+        }
+
+        private void StartStop_Click(object sender, EventArgs e)
+        {
+            StartStop.Text = StartStop.Text.Equals("Start") ? "Stop" : "Start";
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timeNow = new TimeSpan(0, 0, 0);
+            TimeView.Text = timeNow.ToString();
+        }
+    }
+}
